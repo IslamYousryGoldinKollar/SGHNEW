d836585580c56fbb9230e743871d11b35c7c7561
Islam Yossry <islamyossry3@gmail.com>
2025-09-28 02:21:49 +0000

    dont move the pattern down or right or resize it, make it 100% size cen

--- src/components/game/HexMap.tsx
+++ src/components/game/HexMap.tsx
@@ -53,7 +53,7 @@ export default function HexMap({ grid, teams, onHexClick }: HexMapProps) {
                 data-ai-hint="island map"
             />
             <svg viewBox="0 0 2048 2048" className="relative w-full h-full drop-shadow-lg">
-                <g transform="translate(0, 20)">
+                <g transform="translate(-69.45, 171.95)">
                     {hexPaths.map((path, index) => {
                         const square = grid.find(s => s.id === index);
                         const isColored = !!square?.coloredBy;
