93b58ab2ea48773db08d5bd0b09e9e7ac4d28dec
Islam Yossry <islamyossry3@gmail.com>
2025-10-02 10:38:32 +0000

    the superadmin to set to iyossry@gmail.com

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,8 @@
-
 /**
  * A list of Super Admin UIDs who have global access to all sessions.
  * In a production environment, this should be managed securely,
  * for example, through a separate admin management interface or environment variables.
  */
 export const SUPER_ADMIN_UIDS = [
-    'GLdvOzQWorMcsmOpcwvqqZcpCIN2',
-    '40J7xdA4thUfcFf9vGvxUpTfSAD3',
-    'DqPp28DfHAPTibRoMXNoPtj67Mt1',
+    '40J7xdA4thUfcFf9vGvxUpTfSAD3', // Corresponds to iyossry@gmail.com
 ];
